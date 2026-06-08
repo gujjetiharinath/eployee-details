2d3d059547015fbf68b00c630a94eb75d3eaa6c3
gujjetiharinath <your_email@example.com>
2026-06-08 12:55:19 +0530

    Updates

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/react-dev-cards' // Mee file lo unna react plugin untundi
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/eployee-details/',  
-})
